7201692dfb38b8e27682c832c50dc16db6deb749
Martín Cruz <martincruz1426@gmail.com>
2025-05-20 18:50:11 -0300

    refactor: replace hardcoded port with environment variable from config adapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { envs } from '@config/adapters/envs.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.PORT);
 }
 bootstrap();
