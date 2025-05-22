e3f818b2890e453a4f60f563626592bcbbaf167c
Martín Cruz <martincruz1426@gmail.com>
2025-05-22 16:25:58 -0300

    feat: enhance application bootstrap with CORS, global prefix, and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { envs } from '@config/adapters/envs.adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
   await app.listen(envs.PORT);
+  logger.log(`Server running on port ${envs.PORT}`);
 }
 bootstrap();
